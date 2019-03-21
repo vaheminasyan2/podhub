1e49f0ab35c0bc7b718cd347641329b7112c4449
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-20 21:06:53 -0700

    added navbar and route for homepage

---
+++ client/build/precache-manifest.23fdb4785abd0d9001c18c6bb7ac4127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e221576698d59893974d",
+    "url": "/static/js/main.0dfcdded.chunk.js"
+  },
+  {
+    "revision": "4f00ff28a204ede6114a",
+    "url": "/static/js/2.69eeee04.chunk.js"
+  },
+  {
+    "revision": "e221576698d59893974d",
+    "url": "/static/css/main.ed54d332.chunk.css"
+  },
+  {
+    "revision": "1ea6ff5060940ce026a06d4dd9c95e5b",
+    "url": "/index.html"
+  }
+];
--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-success">
-      <Link className="navbarText navbar-brand" to="/">Google Books</Link>
+      <Link className="navbarText navbar-brand" to="/">PodHub</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -16,27 +16,14 @@ function Navbar() {
             <Link
               to="/"
               className={
-                window.location.pathname === "/" || window.location.pathname === "/search"
+                window.location.pathname === "/" || window.location.pathname === "/home"
                   ? "nav-link active"
                   : "nav-link"
               }
             >
-              Search
+              Home
             </Link>
           </li>
-          <li className="nav-item">
-            <Link
-              to="/saved"
-              className={
-                window.location.pathname === "/saved" 
-                  ? "nav-link active" 
-                  : "nav-link"
-              }
-            >
-              Saved
-            </Link>
-          </li>
-
         </div>
       </div>
     </nav>
