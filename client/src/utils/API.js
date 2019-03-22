b50201ea03e886bad4b38bb0d3faf2aed47e33d4
vaheminasyan2 <vaheminasyan2@gmail.com>
2019-03-21 18:11:01 -0700

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,7 @@ export default {
 
     // Gets all posts for specific user
     getPosts: function(userId) {
-        return axios.get("/home", userId);
+        return axios.get("/api/getPosts", userId);
     },
 
     // Gets all episodes for a particular podcast
