4f1d1ad3aa6405dd1a26d29dbfb01b66dca55330
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-03-25 15:07:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("client/build"));
 
 var syncOptions = { force: false };
 
