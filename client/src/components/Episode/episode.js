5e8705d494bf560558c9a12d964209848f0d246f
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-03-26 11:10:54 -0700

    Update episode.js

--- client/src/components/Episode/episode.js
+++ client/src/components/Episode/episode.js
@@ -1,50 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { Redirect } from "react-router";
 import "./episode.css";
 
 // EPISODE COMPONENT
 // This component displays the details of an individual episode.
 // It contains a link that will redirect the user to the Listen to Podcast page.
 
-function Episode({ podcastName, podcastLogo, episodeId, episodeName, date, length, description, audioLink }) {
-  return (
+class Episode extends Component {
+  
+  constructor(props) {
+    super(props); 
+    this.state = { redirect: false };
+  }
 
-    <div className="container rounded-0 border-0 card">
-      <div className="col">
+  listenToEpisode = event => {
+    event.preventDefault();
+    
+    this.setState({
+      redirect: true
+    });
+  }
 
-        {/* Name | Date */}
-        <div className="row">
-          {episodeName} &nbsp;|&nbsp; {date} &nbsp;|&nbsp; {length}
-        </div>
+  render() {
+    
+    if (this.state.redirect) {
+              
+      return ( 
+        <Redirect
+          to={{
+            pathname: "/listen",
+            state: {
+              podcastName: this.props.podcastName,
+              podcastLogo: this.props.podcastLogo,
+              episodeId: this.props.episodeId,
+              episodeName: this.props.episodeName,
+              date: this.props.date,
+              description: this.props.description,
+              audioLink: this.props.audioLink
+            }
+          }}
+        />
+      )
 
-        <div className="border rounded">
+    }
+    
+    return (
 
-          {/* Episode Description */}
-          <span>{description.replace(/<\/?[^>]+(>|$)/g, "")}<br /></span>
+      <div className="container rounded-0 border-0 card">
+        <div className="col">
 
-          {/* Listen Button */}
-          <Link
-            to={{
-              pathname: "/listen",
-              state: {
-                podcastName: podcastName,
-                podcastLogo: podcastLogo,
-                episodeId: episodeId,
-                episodeName: episodeName,
-                date: date,
-                description: description,
-                audioLink: audioLink
-              }
-            }}
-          >
-            Listen
-            </Link>
+          {/* Name | Date */}
+          <div className="row">
+            {this.props.episodeName} &nbsp;|&nbsp; {this.props.date} &nbsp;|&nbsp; {this.props.length}
+          </div>
+
+          <div className="border rounded">
+
+            {/* Episode Description */}
+            <span>{this.props.description.replace(/<\/?[^>]+(>|$)/g, "")}<br /></span>
+
+            <button className="btn btn-primary" onClick={this.listenToEpisode}>Listen</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Episode;
-
-
