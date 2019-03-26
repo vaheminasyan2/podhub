d3c4c2507f8521b5d370749bdcf751a03eaa822f
Lia <liaswapna@gmail.com>
2019-03-25 22:04:28 -0700

    seeds for follow model

--- models/seeds.sql
+++ models/seeds.sql
@@ -2,8 +2,8 @@ USE podHub;
 
 INSERT INTO users(googleId, name, email, profileImage, createdAt, updatedAt) 
 VALUES
-("g123", "Pogos", "pogos@gmail.com", now(), now()),
-("g456", "Petros", "petros@gmail.com", now(), now());
+("g123", "Pogos", "pogos@gmail.com"," ", now(), now()),
+("g456", "Petros", "petros@gmail.com"," ", now(), now());
 
 INSERT INTO posts(title, link, details, message, createdAt, updatedAt, postedBy) 
 VALUES
@@ -14,10 +14,23 @@ VALUES
 
 INSERT INTO comments(comment, createdAt, updatedAt, postId, commentedBy) 
 VALUES
-("comm1", now(), now(), 5, 1),
-("comm1", now(), now(), 5, 2),
-("comm1", now(), now(), 6, 1),
-("comm1", now(), now(), 7, 2);
+("comm1", now(), now(), 1, 1),
+("comm1", now(), now(), 1, 2),
+("comm1", now(), now(), 2, 1),
+("comm1", now(), now(), 3, 2);
+
+INSERT INTO favorites(title,link,imageIcon,details,createdAt,updatedAt,userID)
+VALUES
+("test1","https://www.test1.link"," ","tech talk1",now(), now(),1),
+("test2","https://www.test2.link"," ","tech talk2",now(), now(),1),
+("test3","https://www.test3.link"," ","tech talk3",now(), now(),2);
+
+INSERT INTO follow(isFollowing,followedBy,createdAt,updatedAt)
+VALUES
+(1,2,now(), now()),
+(2,1,now(), now());
+
+
 
 
 /*
