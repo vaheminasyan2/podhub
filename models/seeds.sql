8e788bb41a9c80ae71afa5a170f6c19869c7aff6
Mary Arzumanyan <mary.arzumanyan@gmail.com>
2019-03-26 18:15:02 -0700

    Post and comment likes routes

--- models/seeds.sql
+++ models/seeds.sql
@@ -12,19 +12,17 @@ VALUES
 ("test3", "https://www.test3.link", "fff test3", "test 3 post", now(), now(), 2),
 ("test4", "https://www.test4.link", "ggg test4", "test 4 post", now(), now(), 2);
 
+USE podHub;
 INSERT INTO comments(comment, createdAt, updatedAt, postId, commentedBy) 
 VALUES
-("comm1", now(), now(), 5, 1),
-("comm1", now(), now(), 5, 2),
-("comm1", now(), now(), 6, 1),
-("comm1", now(), now(), 7, 2);
+("comm1", now(), now(), 1, 1),
+("comm1", now(), now(), 1, 2),
+("comm1", now(), now(), 2, 1),
+("comm1", now(), now(), 3, 2);
 
-
-/*
-INSERT INTO posts(title, link, details, message, createdAt, updatedAt) 
+USE podHub;
+INSERT INTO follow(createdAt, updatedAt, isFollowing, followedBy) 
 VALUES
-("test1", "https://www.test1.link", "ttt test1", "test 1 post", now(), now()),
-("test2", "https://www.test2.link", "www test2", "test 2 post", now(), now()),
-("test3", "https://www.test3.link", "fff test3", "test 3 post", now(), now()),
-("test4", "https://www.test4.link", "ggg test4", "test 4 post", now(), now()),
-*/
+(now(), now(), 1, 2),
+(now(), now(), 2, 1);
+
