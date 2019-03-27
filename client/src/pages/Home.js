22940097c18543093862b094d8e9d55427be6e76
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-03-26 23:57:23 -0700

    updated getPost in home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,54 +8,30 @@ import PostCard from "../components/PostCard/postCard";
 class Home extends Component {
 
     state = {
-        posts: [
-            {
-                id:1,
-                userProfileImage: "<image>",
-                userName: "Vahe Minasyan",
-                date: "03/20/2019",
-                message: "Checkout this awesome podcast",
-                podcastIcon: "<image>",
-                podcastEpisode: "Very Bad Wizards Episode 159: You have the right...",
-                episodeDescription: "Description",
-                link: "link",
-                likes: 10,
-                comments: 10,
-            },
-            {
-                id:2,
-                userProfileImage: "<image>",
-                userName: "John Smith",
-                date: "03/21/2019",
-                message: "Checkout this awesome podcast",
-                podcastIcon: "<image>",
-                podcastEpisode: "Very Bad Wizards Episode 159: You have the right...",
-                episodeDescription: "Description",
-                link: "link",
-                likes: 10,
-                comments: 10,
-            },
-        ],
+        posts: [],
         user:[
             {
                 name: "John Smith",
                 userProfileImage: "<image>",
                 googleUserId: "1111",
+                id: 1,
             },
         ],
 
     };
 
-    // componentDidMount() {
-    //     this.getPosts();
-    // };
+    
+
+    componentDidMount() {
+        this.getPosts();
+    };
 
     // Add function to call getPost function every time when something is posted or every 2 mins or so
     
 
     // API request to get the user's and his follower's posts
     getPosts = () => {
-        API.getPosts(this.state.userId)
+        API.getPosts(this.state.user.id)
             .then(res =>
                 this.setState({
                     posts: res.data
@@ -79,10 +55,10 @@ class Home extends Component {
                                 <PostCard
                                     key={post.id}
                                     photo={post.userProfileImage}
-                                    name={post.userName}
-                                    date={post.date}
+                                    name={post.name}
+                                    date={post.createdAt}
                                     message={post.message}
-                                    icon={post.podcastIcon}
+                                    icon={post.imageIcon}
                                     title={post.podcastEpisode}
                                     description={post.episodeDescription}
                                     link={post.link}
