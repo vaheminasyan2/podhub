b007cd059f6eaa2c982dcdcd388a8be9a0e1b889
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-03-30 23:33:34 -0700

    Add files via upload

--- client/src/components/PostCard/postCard.js
+++ client/src/components/PostCard/postCard.js
@@ -3,24 +3,25 @@ import "./postCard.css";
 
 // POST COMPONENT
 
-function Card({ photo, name, date, message, icon, title, description, link, likes, comments }) {
+function Card({ userPhoto, userName, date, podcastName, podcastLogo, episodeName, description, audioLink, userMessage, likes, comments }) {
   return (
     <div className="container rounded-0 border-top-0 border-left-0 border-right-0 card">
       <div className="row">
         <div className="col-1">
-          <img id="profileImage" src={photo} alt="Poster"/>
+          <img id="profileImage" src={userPhoto} alt="User" />
         </div>
         <div className="col">
-          <div className="row">{name} &nbsp;|&nbsp; {date}</div>
-          <div className="row">{message}</div>
+          <div className="row">{userName} &nbsp;|&nbsp; {date}</div>
+          <div className="row">{userMessage}</div>
           <div className="row border rounded">
             <div className="col-2 p-0">
-              <img id="podcastIcon" src={icon} alt="Podcast Icon"/>
+              <img id="podcastIcon" src={podcastLogo} alt="Podcast Logo" />
             </div>
             <div className="col p-0">
-            <p>{title}</p>
-            <p>{description}</p>
-            <a href={link}>{link}</a>
+              <h4>{podcastName}</h4>
+              <p>{episodeName}</p>
+              <p>{description}</p>
+              {/* <a href={audioLink}>{audioLink}</a> */}
             </div>
           </div>
           <div className="row pb-1">
