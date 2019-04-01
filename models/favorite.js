d4ebb3885c64d7cbdda74dffb180afcb4544a103
Lia <liaswapna@gmail.com>
2019-03-31 20:01:15 -0700

    modifiedFavModel

--- models/favorite.js
+++ models/favorite.js
@@ -1,23 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-    var favorite = sequelize.define("favorite", {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-          },
-        link: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            len: [1]
-        },
-        imageIcon: {
-            type: DataTypes.STRING
-        },
-        details: {
-          type: DataTypes.TEXT,
-          allowNull: false
-        }
-    });
-    
-    return favorite;
-  };
-  
+  var favorite = sequelize.define("favorite", {
+    podcastName: {
+      type: DataTypes.STRING(2000),
+      allowNull: false
+    },
+    audioLink: {
+      type: DataTypes.STRING(2000),
+      allowNull: false,
+      len: [1]
+    },
+    podcastLogo: {
+      type: DataTypes.STRING(2000)
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+
+  return favorite;
+};
