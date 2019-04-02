adb5b4e68ca65758fe2b0700630cf7b9b2c92e92
Carson Wack <carson_wack@yahoo.com>
2019-04-01 21:39:47 -0700

    fix

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "root",
-    password: "Allstar5",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "podHub",
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql"
   },
   test: {
