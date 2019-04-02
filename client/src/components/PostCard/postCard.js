c5b3220801b0ca8f974c611bec9d075f3dd61d96
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-04-01 18:52:33 -0700

    css improvements on profile.js and home pages

--- client/src/components/PostCard/postCard.js
+++ client/src/components/PostCard/postCard.js
@@ -5,21 +5,21 @@ import "./postCard.css";
 
 function Card({ userPhoto, userName, date, podcastName, podcastLogo, episodeName, description, audioLink, userMessage, likes, comments, postId, handlePostDelete }) {
   return (
-    <div className="container rounded-0 border-top-0 border-left-0 border-right-0 card">
+    <div className="container border-0 card">
       <div className="row">
         <div className="col-1">
           <img id="profileImage" src={userPhoto} alt="User" />
         </div>
         <div className="col">
-          <div className="row">{userName} &nbsp;|&nbsp; {date}</div>
-          <div className="row">{userMessage}</div>
-          <div className="row border rounded">
+          <div className="row ml-1">{userName} &nbsp;|&nbsp; {date}</div>
+          <div className="row ml-1">{userMessage}</div>
+          <div className="row border-bottom rounded">
             <div className="col-2 p-0">
               <img id="podcastIcon" src={podcastLogo} alt="Podcast Logo" />
             </div>
             <div className="col p-0">
               <button className="btn btn-danger btn-sm deletePost" onClick={() => handlePostDelete(postId)}>Delete</button>
-              <h4>{podcastName}</h4>
+              <h4 className="ml-6">{podcastName}</h4>
               <p>{episodeName}</p>
               <p>{description}</p>
               {/* <a href={audioLink}>{audioLink}</a> */}
