d73021f8ed981bccaed025c263128ee375fa4bff
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-04-02 20:46:31 -0700

    unable modal if there are no likes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -62,25 +62,27 @@ class Home extends Component {
     //Opens the Likes modal
     //Executed upon user clicking "Likes" button on page
     handleShowLikes = postId => {
-        // event.preventDefault();
-        console.log("hello");
-        this.setState({
-            showLikesModal: true
-        });
-
-        //let userId = JSON.parse(localStorage.getItem("user")).id;
 
         API.getLikes(postId).then(res => {
             console.log(res.data);
-            this.setState({
-                likes: res.data
-            });
+            if (res.data.length === 0) {
+                this.setState({
+                    showLikesModal: false
+                });
+            }
+            else {
+                this.setState({
+                    likes: res.data,
+                    showLikesModal: true
+                });
+            }
         });
     };
 
+
     // Closes Likes Episode modal
     // Executed upon user clicking "Likes" button in modal
-    handleCloseLikesModal = () => {
+    closeLikesModal = () => {
         this.setState({
             showLikesModal: false
         });
@@ -90,56 +92,56 @@ class Home extends Component {
         return (
             <div className="container bg-dark rounded">
                 <Row>
-                    {this.state.posts.length > 0 
-                    ? (
-                        <Container>
-                            {this.state.posts.map(post => (
-                                <PostCard
-                                    key={post.id}
-                                    userPhoto={post.userImage}
-                                    userName={post.userName}
-                                    date={moment(post.createdAt).format("LLL")}
-                                    podcastName={post.podcastName}
-                                    podcastLogo={post.podcastLogo}
-                                    episodeName={post.episodeName}
-                                    description={post.description}
-                                    audioLink={post.audioLink}
-                                    userMessage={post.userMessage}
-                                    likes={post.numberOfLikes}
-                                    comments={post.numberOfComments}
-                                    postId={post.id}
-                                    handlePostDelete={this.handlePostDelete}
-                                    handleShowLikes={this.handleShowLikes}
-                                />
-                            ))}
-
-                            <Modal
-                                open={this.state.showLikesModal}
-                                onClose={this.handleCloseLikesModal}
-                                center
-                            >
-                                {this.state.likes.map(like => (
-                                    <div
-                                        className="row rounded favorite bg-dark text-secondary"
-                                        key={like.id}
-                                    >
-                                        <div className="col-3 mt-0">
-                                            <img
-                                                src={like.image}
-                                                alt="User Icon"
-                                                id="userImageLikesModal"
-                                                className="rounded border-white"
-                                            />
-                                        </div>
-                                        <div className="col-9">
-                                            <p>{like.name}</p>
-                                        </div>
-                                    </div>
+                    {this.state.posts.length > 0
+                        ? (
+                            <Container>
+                                {this.state.posts.map(post => (
+                                    <PostCard
+                                        key={post.id}
+                                        userPhoto={post.userImage}
+                                        userName={post.userName}
+                                        date={moment(post.createdAt).format("LLL")}
+                                        podcastName={post.podcastName}
+                                        podcastLogo={post.podcastLogo}
+                                        episodeName={post.episodeName}
+                                        description={post.description}
+                                        audioLink={post.audioLink}
+                                        userMessage={post.userMessage}
+                                        likes={post.numberOfLikes}
+                                        comments={post.numberOfComments}
+                                        postId={post.id}
+                                        handlePostDelete={this.handlePostDelete}
+                                        handleShowLikes={this.handleShowLikes}
+                                    />
                                 ))}
-                            </Modal>
-                        </Container>
-                        ) 
-                    : 
+
+                                <Modal
+                                    open={this.state.showLikesModal}
+                                    onClose={this.closeLikesModal}
+                                    center
+                                >
+                                    {this.state.likes.map(like => (
+                                        <div
+                                            className="row rounded favorite bg-dark text-secondary"
+                                            key={like.id}
+                                        >
+                                            <div className="col-3 mt-0">
+                                                <img
+                                                    src={like.image}
+                                                    alt="User Icon"
+                                                    id="userImageLikesModal"
+                                                    className="rounded border-white"
+                                                />
+                                            </div>
+                                            <div className="col-9">
+                                                <p>{like.name}</p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </Modal>
+                            </Container>
+                        )
+                        :
                         (
                             <h4 className="text-center">{this.state.message}</h4>
                         )}
