c81dc5f139200759c595f32412689894116725df
Lia <liaswapna@gmail.com>
2019-04-03 14:12:29 -0700

    commentAndcommentLikesRoute

--- routes/api/commentRoute.js
+++ routes/api/commentRoute.js
@@ -10,6 +10,13 @@ const controller = new CommentController();
 router.post("/", (req, res) => controller.createComment(req, res));
 
 /**
+ * Route to create likes for comment in database
+ * @param {*} req
+ * @param {*} res
+ */
+router.post("/commentLikes", (req, res) => controller.createCommentLikes(req, res));
+
+/**
  * Route to update the existing comment
  * @param {*} req
  * @param {*} res
@@ -22,6 +29,13 @@ router.put("/:id", (req, res) => controller.update(req, res));
  * @param {*} res
  */
 router.delete("/:id", (req, res) => controller.removeComment(req, res));
+
+/**
+ * Route to remove the likes from comments
+ * @param {*} req
+ * @param {*} res
+ */
+router.delete("/commentUnlikes/:id", (req, res) => controller.removeCommentLikes(req, res));
 
 /**
  * Get Comment likes
