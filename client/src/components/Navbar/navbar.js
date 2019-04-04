ce2231fd3a0ad9afa290e59633c480282afae522
Carson Wack <carson_wack@yahoo.com>
2019-04-04 10:32:56 -0700

    scooted logo; changed navbar.js to take state & methods; got navbar audio player controls working

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,109 +10,126 @@ import AudioPlayer from "../AudioPlayer/audioPlayer";
 
 library.add(faSearch, faUser, faHome);
 
-function Navbar({ podcastSearch, handleInputChange, hidePodcasts, logout, user, showAudio, hideAudio }) {
+class Navbar extends Component {
 
-  return (
+  state = {
+    speed: 1.0,
+  };
 
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-      <div className="container mini">
-      <Link className="navbarText navbar-brand" to="/home"><img src={Logo} alt="logo" id="size"/></Link>
+  changeSpeed = (event) => {
+    this.setState({
+      speed: event.target.value
+    });
+  }
 
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+  render() {
+    const { podcastSearch, handleInputChange, hidePodcasts, logout, user, showAudio, hideAudio } = this.props;
+    return (
 
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-        <ul className="navbar-nav mr-auto">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container mini">
+          <Link className="navbarText navbar-brand" to="/home"><img src={Logo} alt="logo" id="size" /></Link>
 
-          <li className="nav-item">
-            <Link
-              to="/home"
-              className={
-                window.location.pathname === "/home"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-            <FontAwesomeIcon icon="home" />
-            &nbsp; Home
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+            <ul className="navbar-nav mr-auto">
+
+              <li className="nav-item">
+                <Link
+                  to="/home"
+                  className={
+                    window.location.pathname === "/home"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  <FontAwesomeIcon icon="home" />
+                  &nbsp; Home
             </Link>
-          </li>
+              </li>
 
-          <li className="nav-item">
-            <Link
-              to={{
-                pathname: "/profile",
-                state: {
-                  user: user
-                }
-              }}
-              className={
-                window.location.pathname === "/profile"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              <FontAwesomeIcon icon="user" />
-              &nbsp; Profile
+              <li className="nav-item">
+                <Link
+                  to={{
+                    pathname: "/profile",
+                    state: {
+                      user: user
+                    }
+                  }}
+                  className={
+                    window.location.pathname === "/profile"
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  <FontAwesomeIcon icon="user" />
+                  &nbsp; Profile
             </Link>
-          </li>
+              </li>
 
-          <li className="nav-item">
-            <Link
-              to={{
-                pathname: "/userSearch",
-              }}
-              className="nav-link"
+              <li className="nav-item">
+                <Link
+                  to={{
+                    pathname: "/userSearch",
+                  }}
+                  className="nav-link"
+                >
+                  <FontAwesomeIcon icon="search" />
+                  <span>&nbsp; Find Users</span>
+                </Link>
+              </li>
+            </ul>
+
+            {sessionStorage.getItem("audioSettings") && showAudio ? (
+              <span>
+                <AudioPlayer
+                  audioLink={JSON.parse(sessionStorage.getItem("audioSettings")).audioLink}
+                  playbackRate={this.state.speed}
+                  changeSpeed={this.changeSpeed}
+                  initialSpeed={this.state.speed}
+                />
+                <button className="btn btn-dark btn-sm" onClick={hideAudio}>Hide</button>
+              </span>
+
+            ) : (
+                <></>
+              )}
+
+            <ul className="navbar-nav">
+              <form className="form-inline my-2 my-lg-0 searchPodcastForm">
+                <input className="form-control mr-sm-2 searchPodcastInput"
+                  type="search"
+                  placeholder="Search Podcasts"
+                  aria-label="Search"
+                  id="podcastInput"
+                  value={podcastSearch}
+                  name="podcastSearch"
+                  autoComplete="off"
+                  onBlur={hidePodcasts}
+                  onChange={handleInputChange}
+                  onFocus={handleInputChange}
+                  required
+                />
+              </form>
+            </ul>
+
+            <button
+              onClick={logout}
+              className="logoutButton btn btn-dark"
             >
-              <FontAwesomeIcon icon="search" />
-              <span>&nbsp; Find Users</span>
-            </Link>
-          </li>
-        </ul>
-
-        {sessionStorage.getItem("audioSettings") && showAudio ? (
-            <span>
-              <AudioPlayer 
-                audioLink={JSON.parse(sessionStorage.getItem("audioSettings")).audioLink}
-              />
-              <button className="btn btn-dark btn-sm" onClick={hideAudio}>Hide</button>
-            </span>
-
-        ) : (
-          <></>
-        )}
-
-        <ul className="navbar-nav">
-          <form className="form-inline my-2 my-lg-0 searchPodcastForm">
-            <input className="form-control mr-sm-2 searchPodcastInput"
-              type="search"
-              placeholder="Search Podcasts"
-              aria-label="Search"
-              id="podcastInput"
-              value={podcastSearch}
-              name="podcastSearch"
-              autoComplete="off"
-              onBlur={hidePodcasts}
-              onChange={handleInputChange}
-              onFocus={handleInputChange}
-              required
-            />
-          </form>
-        </ul>
-
-        <button
-          onClick={logout}
-          className="logoutButton btn btn-dark"
-        >
-        Logout
+              Logout
         </button>
 
-      </div>
-            </div>
-    </nav>
-  );
-}
+          </div>
+        </div>
+      </nav>
+    )
+  }
+};
 
 export default Navbar;
