7f57673992e7d422e9ef1d30a9edccab5d4f8aee
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-04-03 23:46:13 -0700

    enabled comment delete functionality and show comment likes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,9 +7,13 @@ import PostCard from "../components/PostCard/postCard";
 import Modal from "react-responsive-modal";
 import "./Home.css";
 import Delete from "../pages/delete.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons'
 import User from "../components/User/user";
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faComment);
+library.add(faHeart);
+
 
 let moment = require("moment");
 
@@ -90,7 +94,7 @@ class Home extends Component {
         API.likePost(postId, this.props.user.id).then(res => {
             //console.log(res.data)
             if (res.data[1] === false) {
-                API.unlikePost(postId,this.props.user.id).then(res => {
+                API.unlikePost(postId, this.props.user.id).then(res => {
                     //console.log(res.data)
                 })
             };
@@ -138,7 +142,8 @@ class Home extends Component {
             API.deleteComment(commentId).then(res => {
                 console.log(res.data)
                 this.getPosts();
-                //this.handleShowComments();
+                this.handleShowComments();
+                this.closeCommentsModal();
             });
         }
     };
@@ -153,20 +158,20 @@ class Home extends Component {
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
-          [name]: value
+            [name]: value
         });
-      };
+    };
 
     followUser = (id) => {
         API.followUser(this.props.user.id, id)
-            .then(function(response){
+            .then(function (response) {
                 console.log(response);
                 alert("Followed!");
             })
-             .catch((err) =>
-                 console.log(err)
-                )
-    }            
+            .catch((err) =>
+                console.log(err)
+            )
+    }
 
     render() {
         console.log(this.state.currentComment)
@@ -222,13 +227,14 @@ class Home extends Component {
                                             <div className="col-9">
                                                 <p>{like.name}</p>
                                                 <button
-                                                    className="btn btn-outline-light bPosition" 
-                                                     onClick={(event)=>{
-                                                     event.preventDefault();
-                                                    this.followUser(like.id)}
-                                                 }
-                                                 >
-                                                 Follow
+                                                    className="btn btn-outline-light bPosition"
+                                                    onClick={(event) => {
+                                                        event.preventDefault();
+                                                        this.followUser(like.id)
+                                                    }
+                                                    }
+                                                >
+                                                    Follow
                                                 </button>
                                             </div>
                                         </div>
@@ -253,18 +259,9 @@ class Home extends Component {
                                                         className="rounded border-white mt-1"
                                                     />
                                                 </div>
-                                                <div className="col-9">
+                                                <div className="col-10">
                                                     <p>{comment.userName}&nbsp;|&nbsp; {moment(comment.createdAt).format("LLL")}</p>
                                                 </div>
-                                                {this.props.user.id === comment.commentedBy 
-                                                ?
-                                                <div className="col-1">
-                                                    <button className="btn btn-sm deleteComment float-right" onClick={() => this.deleteComment(comment.id)}>
-                                                        <img src={Delete} alt="delete" className="x" />
-                                                    </button>
-                                                </div>
-                                               : null
-                                            }
                                             </div>
 
                                             <div
@@ -272,16 +269,41 @@ class Home extends Component {
                                             >
                                                 <p className="userComment pl-2 ml-3">{comment.comment}</p>
                                             </div>
+                                            <div className="row comment-third-row">
+                                            <div className="col-4 mb-2">
+                                                <a
+                                                    className="likes ml-4"
+                                                // onClick={() => handleLikeOrUnlike(postId)}
+                                                >
+                                                    <FontAwesomeIcon icon="heart" />
+                                                </a>
+                                                <a
+                                                    className="likesNumber"
+                                                // onClick={() => handleShowLikes(postId)}
+                                                >
+                                                    {comment.numberOfLikes}
+                                                </a>
+                                                </div>
+                                                {this.props.user.id === comment.commentedBy
+                                                    ?
+                                                <div className="col-8">
+                                                <button className="btn btn-sm deleteComment float-right" onClick={() => this.deleteComment(comment.id)}>
+                                                    Remove
+                                                </button>
+                                                </div>
+                                                          : null
+                                                        }
+                                            </div>
                                         </div>
                                     ))}
 
                                     <form>
                                         <div className="form-group mt-4 bg-dark text-secondary">
                                             <input type="text" className="form-control" id="commentForm"
-                                            defaultValue=""
-                                            placeholder="Enter your comment" ref={this.state.currentComment} onChange={this.handleInputChange}/>
+                                                defaultValue=""
+                                                placeholder="Enter your comment" ref={this.state.currentComment} onChange={this.handleInputChange} />
                                         </div>
-                                        <button type="submit" className="btn btn-light btn-sm mb-2" onClick={(event) => {event.preventDefault(); this.addComment(this.props.postId)}
+                                        <button type="submit" className="btn btn-light btn-sm mb-2" onClick={(event) => { event.preventDefault(); this.addComment(this.props.postId) }
                                         }
                                         >Submit</button>
                                     </form>
