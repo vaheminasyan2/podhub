69ea28fef462e0f33db135b5549f4a55db3363dc
Lia <liaswapna@gmail.com>
2019-04-04 23:48:14 -0700

    commentLikesRefreshFix

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -210,8 +210,9 @@ showFollowingModal = () => {
       if (res.data[1] === false) {
         API.unlikePost(postId, this.state.user.id).then(res => {
           //console.log(res.data)
+          
         })
-      };
+      } 
       this.getPostsOnlyByUser();
     })
   }
@@ -237,10 +238,13 @@ showFollowingModal = () => {
       if (res.data[1] === false) {
         API.unlikeComment(commentId, this.state.user.id).then(res => {
           // console.log(res.data)
+          this.handleShowComments(this.state.currentPostId);
         })
-      };
-      this.getPostsOnlyByUser();
-      this.handleShowComments();
+      }else{
+        this.handleShowComments(this.state.currentPostId);
+      }
+      // this.getPostsOnlyByUser();
+      // this.handleShowComments();
     })
   }
 
