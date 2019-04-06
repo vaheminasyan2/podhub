32bcea94805d57b95f329ef5722508240bb137a8
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-05 21:40:17 -0700

    login page font/spacing adjustments

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -47,8 +47,13 @@ class Login extends Component {
                     <br/><br/>
                     <div className="sizing">
                     <img src={Logo} alt="logo" className="autoM"/>
-                    <h1 id="loginPageTitle" className="text-center font-weight-bold">Welcome to PodHub</h1>
-                    <h4 id="slogan" className="text-center font-weight-bold">Share what you care about</h4>
+            
+                    <br />
+
+                    <div class="loginText">
+                        <h1 id="loginPageTitle" className="text-center font-weight-bold">PodHub</h1>
+                        <h4 id="slogan" className="text-center font-weight-bold">Share what you care about</h4>
+                    </div>
                     </div>
                 </div>
             </Container>
