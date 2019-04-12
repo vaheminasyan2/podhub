84486503b10bd41e1dc27a7a8e949f6f57e83cb3
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-12 12:41:54 -0700

    integrate profile header component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,10 +6,7 @@ import API from "../utils/API";
 import Post from "../components/Post/post";
 import Delete from "./delete-1.png";
 import moment from "moment";
-import Modal from "react-responsive-modal";
-import User from "../components/User/user";
-import List from "../components/List/list";
-import AboutMe from "../components/AboutMe/aboutMe";
+import ProfileHeader from "../components/ProfileHeader/profileHeader";
 import "./Profile.css";
 
 // USER PROFILE PAGE
@@ -17,35 +14,21 @@ import "./Profile.css";
 // Contains info on user's # of posts, followers, and followings
 // Displays user's favorites
 // Displays user's posts
-// Has functionality to like/unlike a post
-// Has functionality to comment on post
-// Has functionality to like/unlike a comment
-// Has modals to display: likes, comments, followers, followings
-// Has functionality to redirect to Episode List and Listen page on post click
 
 class Profile extends Component {
   state = {
     user: [],
-    userIsFollowed: null,
     posts: [],
-    numFollowers: 0,
-    numFollowing: 0,
     favorites: [],
     currentPostId: "",
-    followers: [],
-    following: [],
-    showFollowersModal: false,
-    showFollowingModal: false,
-    redirect: false
+    redirect: false,
+    message: ""
   };
 
   // Load user profile information
   componentDidMount() {
     this.getFavorites();
     this.getPostsOnlyByUser();
-    this.getNumFollowers();
-    this.getNumFollowing();
-    this.isUserFollowed();
     this.setState({
       user: this.props.location.state.user
     });
@@ -54,25 +37,16 @@ class Profile extends Component {
   // Update profile information if subject user changes
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.location.state.user.id !== this.props.location.state.user.id) {
-
       this.getFavorites();
       this.getPostsOnlyByUser();
-      this.getNumFollowers();
-      this.getNumFollowing();
-      this.isUserFollowed();
       this.setState({
         user: this.props.location.state.user
       });
     }
-
-    if (prevState.userIsFollowed !== this.state.userIsFollowed) {
-      this.getNumFollowers();
-      this.getNumFollowing();
-    }
   }
 
 
-  // POPULATE USER PROFILE INFORMATION
+  // POPULATE POST & FAVORITE INFORMATION
   // ===============================================
 
   // Get user data and set it in state
@@ -84,36 +58,6 @@ class Profile extends Component {
     });
   };
 
-  // Get number of FOLLOWERS for user
-  getNumFollowers = () => {
-    API.getFollowers(this.props.location.state.user.id)
-      .then(res => {
-        this.setState({
-          numFollowers: res.data[0].count
-        });
-      })
-      .catch(() => {
-        this.setState({
-          numFollowers: 0
-        });
-      });
-  };
-
-  // Get number of other users that current user is FOLLOWING
-  getNumFollowing = () => {
-    API.getFollowing(this.props.location.state.user.id)
-      .then(res => {
-        this.setState({
-          numFollowing: res.data[0].count
-        });
-      })
-      .catch(() => {
-        this.setState({
-          numFollowing: 0
-        });
-      });
-  };
-
   // Get user's FAVORITES
   getFavorites = () => {
     API.getFavorites(this.props.location.state.user.id)
@@ -121,7 +65,7 @@ class Profile extends Component {
         if (res.data.length === 0) {
           this.setState({
             favorites: [],
-            messageNoFav:
+            message:
               "No favorites found."
           });
         } else {
@@ -133,7 +77,7 @@ class Profile extends Component {
       .catch(() => {
         this.setState({
           favorites: [],
-          messageNoFav:
+          message:
             "No favorites found."
         });
       });
@@ -146,7 +90,7 @@ class Profile extends Component {
         if (res.data.length === 0) {
           this.setState({
             posts: [],
-            messageNoPodcast: "No posts found."
+            message: "No posts found."
           });
         } else {
           this.setState({
@@ -157,56 +101,10 @@ class Profile extends Component {
       .catch(() => {
         this.setState({
           posts: [],
-          messageNoPodcast: "No posts found."
+          message: "No posts found."
         });
       });
   };
-
-
-  // LIST OF FOLLOWERS / FOLLOWINGS, MODALS
-  // ===============================================
-
-  // Get list of user's followers
-  getFollowers = () => {
-    API.isFollowedByUsers(this.state.user.id)
-      .then(res => {
-        this.setState({
-          followers: res.data,
-        }, () => { this.showFollowersModal() });
-      });
-  }
-
-  // Get list of other users that user is following
-  getUsersFollowed = () => {
-    API.getUsersFollowed(this.state.user.id)
-      .then(res => {
-        this.setState({
-          following: res.data
-        }, () => { this.showFollowingModal() });
-      });
-  }
-
-  // Show modal that displays followers
-  showFollowersModal = () => {
-    this.setState({
-      showFollowersModal: true
-    });
-  }
-
-  // Show modal that displays other users being followed
-  showFollowingModal = () => {
-    this.setState({
-      showFollowingModal: true
-    });
-  }
-
-  // Hide Followers and Followings modals
-  hideFollowersModal = () => {
-    this.setState({
-      showFollowersModal: false,
-      showFollowingModal: false
-    });
-  }
 
 
   // POST AND FAVORITE FUNCTIONALITY
@@ -230,70 +128,6 @@ class Profile extends Component {
   };
 
 
-  // FOLLOW / UNFOLLOW USER
-  // ===============================================
-
-  // Checks to see if user is following viewed user
-  isUserFollowed = () => {
-
-    // Get current user's ID
-    let currUserId = JSON.parse(localStorage.getItem("user")).id;
-
-    // Get list of users followed by current user
-    API.getUsersFollowed(currUserId)
-      .then(res => {
-
-        let usersFollowed = res.data;
-
-        // Look for viewed user's ID in list of followed users
-        usersFollowed.forEach(element => {
-          if (this.state.user.id === element.id) {
-
-            this.setState({
-              userIsFollowed: true
-            });
-
-            return;
-          }
-        });
-      });
-  }
-
-  // Follows user if follow button is clicked
-  followUser = (userId) => {
-
-    let that = this;
-    let currUserId = JSON.parse(localStorage.getItem("user")).id;
-
-    API.followUser(currUserId, userId)
-      .then(function (response) {
-        that.setState({
-          userIsFollowed: true
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  // Unfollows user if unfollow button is clicked
-  unfollowUser = (userId) => {
-
-    let that = this;
-    let currUserId = JSON.parse(localStorage.getItem("user")).id;
-
-    API.unFollowUser(currUserId, userId)
-      .then(function (response) {
-        that.setState({
-          userIsFollowed: false
-        });
-      })
-      .catch((err) =>
-        console.log(err)
-      )
-  }
-
-
   // OTHER
   // ===============================================
 
@@ -304,16 +138,10 @@ class Profile extends Component {
     });
   };
 
-  // Scrolls to post section when Posts is clicked from profile header
-  scrollTo = () => {
-    window.scrollTo(0, 500);
-  }
-
   // Takes (True, Audio Link) and passes them to App.js
   toHomeAndProfile = (value, link, podName, epName) => {
     this.props.toApp(value, link, podName, epName);
-}
-
+  }
 
   render() {
     return (
@@ -323,144 +151,11 @@ class Profile extends Component {
           <div className="col-md-8 col-xs-12">
             <Container>
 
-              <div className="row userProfile rounded bg-dark text-white">
-                <div className="col-3">
-                  <img
-                    src={this.props.location.state.user.profileImage}
-                    alt="User"
-                    id="userMainProfileImage"
-                    className="rounded border-white"
-                  />
-                </div>
+              {/* PROFILE HEADER */}
 
-                <div className="col">
-
-                  {/* User Name */}
-
-                  <Row>
-                    <h2 className="paddingTop userName">{this.props.location.state.user.name}</h2>
-                  </Row>
-
-                  {/* Follow Button */}
-
-                  {this.state.user.id !== JSON.parse(localStorage.getItem("user")).id ? (
-                    this.state.userIsFollowed ? (
-                      <button
-                        className="btn btn-outline-light followBtn"
-                        onClick={(event) => { event.preventDefault(); this.unfollowUser(this.state.user.id) }}
-                      >
-                        Unfollow
-                        </button>
-                    ) : (
-                        <button
-                          className="btn btn-outline-light followBtn"
-                          onClick={(event) => { event.preventDefault(); this.followUser(this.state.user.id) }}
-                        >
-                          Follow
-                        </button>
-                      )
-
-                  ) : (
-                      <></>
-                    )
-                  }
-
-                  {/* User Info: Posts, Followers, Following */}
-
-                  <Row>
-                    <div className="btn btn-dark postsBtn" onClick={this.scrollTo}>
-                      Posts:&nbsp; {this.state.posts.length}
-                    </div>
-
-                    <button
-                      className="btn btn-dark"
-                      onClick={this.getFollowers}
-                    >
-                      Followers:&nbsp;{this.state.numFollowers}
-                    </button>
-
-                    {/* FOLLOWERS MODAL */}
-
-                    <Modal
-                      open={this.state.showFollowersModal}
-                      onClose={this.hideFollowersModal}
-                      classNames={{ modal: "followersModal" }}
-                    >
-                      <h4 className="modalTitle">Followers</h4>
-
-                      {this.state.followers.length ? (
-                        <List>
-                          {this.state.followers.map(user =>
-                            <div className="container tile m-2 userList" key={user.id}>
-                              <User
-                                userId={user.id}
-                                userName={user.name}
-                                userImage={user.image}
-                                handler={this.hideFollowersModal}
-                              />
-                            </div>
-                          )}
-                        </List>
-                      ) : (
-                          this.state.message !== "Loading..." ? (
-                            <h2>No followers found.</h2>
-                          ) : (
-                              <></>
-                            )
-                        )}
-
-                      <h2>{this.state.message}</h2>
-
-                    </Modal>
-
-                    <button
-                      className="btn btn-dark"
-                      onClick={this.getUsersFollowed}
-                    >
-                      Following:&nbsp;{this.state.numFollowing}
-                    </button>
-
-                    {/* FOLLOWING MODAL */}
-
-                    <Modal
-                      open={this.state.showFollowingModal}
-                      onClose={this.hideFollowersModal}
-                      classNames={{ modal: "followersModal" }}
-                    >
-                      <h4 className="modalTitle">Following</h4>
-
-                      {this.state.following.length ? (
-                        <List>
-                          {this.state.following.map(user =>
-                            <div className="container tile m-2 userList" key={user.id}>
-                              <User
-                                userId={user.id}
-                                userName={user.name}
-                                userImage={user.profileImage}
-                                handler={this.hideFollowersModal}
-                              />
-                            </div>
-                          )}
-                        </List>
-                      ) : (
-                          this.state.message !== "Loading..." ? (
-                            <h2>User is not following anyone.</h2>
-                          ) : (
-                              <></>
-                            )
-                        )}
-
-                      <h2>{this.state.message}</h2>
-
-                    </Modal>
-                  </Row>
-                </div>
-              </div>
-
-              {/* ABOUT ME SECTION */}
-
-              <AboutMe 
+              <ProfileHeader 
                 user={this.props.location.state.user}
+                numPosts={this.state.posts.length}
               />
 
               {/* FAVORITES SECTION */}
@@ -547,7 +242,7 @@ class Profile extends Component {
                   </Container>
                 ) : (
                     <div className="col">
-                      <h5 className="text-center">&nbsp;{this.state.messageNoFav}</h5>
+                      <h5 className="text-center">&nbsp;{this.state.message}</h5>
                     </div>
                   )}
               </div>
@@ -585,7 +280,7 @@ class Profile extends Component {
                 ) : (
                     <div className="col">
                       <h5 className="text-center">
-                        &nbsp;{this.state.messageNoPodcast}
+                        &nbsp;{this.state.message}
                       </h5>
                     </div>
                   )}
