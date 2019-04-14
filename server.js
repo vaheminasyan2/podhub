2085348bedcac485a4fa4bae65d73d4e8e82fbd7
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-04-13 21:03:56 -0700

    hid the google signIn client id

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-console.log(process.env.NODE_ENV);
+//console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
