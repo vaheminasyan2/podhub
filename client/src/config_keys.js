d14bf41384979452475a7f7015953d67e62b320e
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-04-16 21:55:29 -0700

    Delete config_keys.js

--- client/src/config_keys.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-    googleClientId:"940323765774-bpnsf77f8vksurn7gbv082gatubu97kl.apps.googleusercontent.com",
-    listenNotesAPIKey:"a063bce4f1msh0a4f44209d57a2fp1225adjsn3f80cc1cf1bb"
-}
-
-export default secrets;
-
