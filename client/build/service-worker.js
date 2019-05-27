0fd5c336af96ec70ab3b5fdb6a62dd0e53941647
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-05-27 12:18:55 -0700

    npm run build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce5a7289555dff9f792ab92fb748e163.js"
+  "/precache-manifest.1b3e360cd2797df689d966bafe6b5b1d.js"
 );
 
 workbox.clientsClaim();
