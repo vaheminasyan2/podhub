e2258ce73a47b16977b42ea20de2d05d103dd506
Lia <liaswapna@gmail.com>
2019-05-27 11:12:11 -0700

    get-route-updates

--- routes/api/awsRoute.js
+++ routes/api/awsRoute.js
@@ -3,10 +3,21 @@ const AWSController = require("../../controllers/AWSController");
 const controller = new AWSController();
 
 /**
- * Route to create a new comment in database
+ * Route to upload image to AWS S3
  * @param {*} req
  * @param {*} res
-*/
-router.post("/awsImageUpload/:userId", (req, res) => controller.awsUploadImage(req, res));
+ */
+router.post("/awsImageUpload/:userId", (req, res) =>
+  controller.awsUploadImage(req, res)
+);
+
+/**
+ * Route to get image url from AWS S3
+ * @param {*} req
+ * @param {*} res
+ */
+router.get("/awsGetImageUrl/:userId", (req, res) =>
+  controller.awsGetImageUrl(req, res)
+);
 
 module.exports = router;
