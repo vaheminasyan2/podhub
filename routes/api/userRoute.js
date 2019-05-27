348855b165b516e558a6d44e51e060aaeb52c906
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-05-27 16:44:44 -0700

    change getAboutMe route to getProfileHeader

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -23,12 +23,12 @@ router.post("/", (req, res) => {
 });
 
 /**
- * Route to get user About Me section
+ * Route to get user Profile Header section
  * @param {*} req
  * @param {*} res
  */
-router.get("/getAboutMe/:googleId", (req, res) => {
-    controller.getAboutMe(req, res);
+router.get("/getProfileHeader/:googleId", (req, res) => {
+    controller.getProfileHeader(req, res);
 });
 
 /**
